c221d65f30e26e6cfd7c5b77f08892ccdb503b57
shanisharrma <shanisharrma13@gmail.com>
2024-12-31 15:59:44 +0530

    feat: Add Elasticsearch APM, Heartbeat, Metricbeat configurations

--- microservices/2-notification-service/src/config.ts
+++ microservices/2-notification-service/src/config.ts
@@ -2,6 +2,20 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+if(process.env.ENABLE_APM === '1') {
+  require('elastic-apm-node').start({
+    serviceName: 'hustlr-notification',
+    serverUrl: process.env.ELASTIC_APM_SERVER_URL,
+    secretToken: process.env.ELASTIC_APM_SECRET_TOKEN,
+    environment: process.env.NODE_ENV,
+    active: true,
+    // active: process.env.NODE_ENV !== 'development',
+    captureBody: 'all',
+    errorOnAbortedRequests: true,
+    captureErrorLogStackTraces: 'always',
+  })
+}
+
 class Config {
   public NODE_ENV: string | undefined;
   public CLIENT_URL: string | undefined;
--- microservices/5-gig-service/src/config/server.config.ts
+++ microservices/5-gig-service/src/config/server.config.ts
@@ -3,6 +3,20 @@ import cloudinary from 'cloudinary';
 
 dotenv.config();
 
+if(process.env.ENABLE_APM === '1') {
+  require('elastic-apm-node').start({
+    serviceName: 'hustlr-gig',
+    serverUrl: process.env.ELASTIC_APM_SERVER_URL,
+    secretToken: process.env.ELASTIC_APM_SECRET_TOKEN,
+    environment: process.env.NODE_ENV,
+    active: true,
+    // active: process.env.NODE_ENV !== 'development',
+    captureBody: 'all',
+    errorOnAbortedRequests: true,
+    captureErrorLogStackTraces: 'always',
+  })
+}
+
 class ServerConfig {
   public NODE_ENV: string | undefined;
   public SERVER_PORT: string | undefined;
